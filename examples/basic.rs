82b9c2345e972de745852057710cf1c5d2d9d788
Isaac Whitfield <iw@whitfin.io>
2019-03-28 11:14:13 -0700

    Update examples alongside the README

---
+++ examples/basic.rs
@@ -0,0 +1,23 @@
+use usher::prelude::*;
+
+fn main() {
+    // First we construct our `Router` using a set of parsers. Fortunately for
+    // this example, Usher includes the `StaticParser` which uses basic string
+    // matching to determine whether a segment in the path matches.
+    let mut router: Router<String> = Router::new(vec![
+        Box::new(StaticParser),
+    ]);
+
+    // Then we insert our routes; in this case we're going to store the numbers
+    // 1, 2 and 3, against their equivalent name in typed English (with a "/"
+    // as a prefix, as Usher expects filesystem-like paths (for now)).
+    router.insert("/one", "1".to_string());
+    router.insert("/two", "2".to_string());
+    router.insert("/three", "3".to_string());
+
+    // Finally we'll just do a lookup on each path, as well as the a path which
+    // doesn't match ("/"), just to demonstrate what the return types look like.
+    for path in vec!["/", "/one", "/two", "/three"] {
+        println!("{}: {:?}", path, router.lookup(path));
+    }
+}
--- examples/http.rs
+++ examples/http.rs
@@ -2,27 +2,55 @@ use http::Method;
 use usher::ext::http::HttpRouter;
 use usher::prelude::*;
 
-#[derive(Debug)]
-pub struct DynamicMatcher(String);
+/// A `Matcher` type used to match against dynamic segments.
+///
+/// The internal value here is the name of the path parameter (based on the
+/// example talked through above, this would be the _owned_ `String` of `"id"`).
+pub struct DynamicMatcher {
+    inner: String
+}
+
 impl Matcher for DynamicMatcher {
+    /// Determines if there is a capture for the incoming segment.
+    ///
+    /// In the pattern we described above the entire value becomes the capture,
+    /// so we return a tuple of `("id", <segment>)` to represent the capture.
     fn capture<'a>(&self, segment: &'a str) -> Option<(&str, &'a str)> {
         Some((&self.0, segment))
     }
 
+    /// Determines if this matcher matches the incoming segment.
+    ///
+    /// Because the segment is dynamic and matches any value, this is able to
+    /// always return `true` without even looking at the incoming segment.
     fn is_match(&self, _segment: &str) -> bool {
         true
     }
 }
 
-#[derive(Debug)]
+/// A `Parser` type used to parse out `DynamicMatcher` values.
 pub struct DynamicParser;
+
 impl Parser for DynamicParser {
+    /// Attempts to parse a segment into a corresponding `Matcher`.
+    ///
+    /// As a dynamic segment is determined by the pattern `:.+`, we check the first
+    /// character of the segment. If the segment is not `:` we are unable to parse
+    /// and so return a `None` value.
+    ///
+    /// If it does start with a `:`, we construct a `DynamicMatcher` and pass the
+    /// parameter name through as it's used when capturing values.
     fn parse(&self, segment: &str) -> Option<Box<Matcher>> {
         if &segment[0..1] != ":" {
             return None;
         }
-        let name = (&segment[1..]).to_owned();
-        Some(Box::new(DynamicMatcher(name)))
+
+        let field = &segment[1..];
+        let matcher = DynamicMatcher {
+            inner: field.to_owned()
+        };
+
+        Some(Box::new(matcher))
     }
 }
 
